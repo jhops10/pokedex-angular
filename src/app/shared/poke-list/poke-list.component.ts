f6db0aad5ca16efa1a430976873d0ac2ae5bd876
João Paulo <joaowais@gmail.com>
2023-10-19 16:07:06 -0300

    Populando lista pokemon através do service poke-api

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
-  styleUrls: ['./poke-list.component.scss']
+  styleUrls: ['./poke-list.component.scss'],
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  public getAllPokemons: any;
 
+  constructor(private pokeApiService: PokeApiService) {}
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe((res) => {
+      this.getAllPokemons = res.results;
+    });
+  }
 }
