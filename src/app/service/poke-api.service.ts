e0157e445914d1246cb3f57a59a9900761f650f2
João Paulo <joaowais@gmail.com>
2023-10-21 10:22:44 -0300

    Busca finalizada

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -7,7 +7,7 @@ import { Observable, map, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class PokeApiService {
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10';
 
   constructor(private http: HttpClient) {}
 
