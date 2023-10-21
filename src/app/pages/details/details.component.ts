679e8fadb04fce7ae5d1bdeb510ebe522f23828c
João Paulo <joaowais@gmail.com>
2023-10-21 10:55:12 -0300

    Adicionado estrutura e css da pagina details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  get getPokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      this.pokemon = res;
+    });
+  }
 }
